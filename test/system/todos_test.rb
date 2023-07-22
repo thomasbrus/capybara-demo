60d190fee409770fab47109c7cc04b92d4d02325
Thomas Brus <thomas@bookingexperts.com>
2023-07-22 16:29:32 +0200

    Set up todos system test

--- test/system/todos_test.rb
+++ test/system/todos_test.rb
@@ -1,47 +1,45 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class TodosTest < ApplicationSystemTestCase
-  setup do
-    @todo = todos(:one)
-  end
+  setup { @todo = todos(:one) }
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit todos_url
-    assert_selector "h1", text: "Todos"
+    assert_selector 'h1', text: 'Todos'
   end
 
-  test "should create todo" do
+  test 'should create todo' do
     visit todos_url
-    click_on "New todo"
+    click_on 'New todo'
 
-    fill_in "Deadline", with: @todo.deadline
-    fill_in "Description", with: @todo.description
-    fill_in "Finished at", with: @todo.finished_at
-    fill_in "Title", with: @todo.title
-    click_on "Create Todo"
+    fill_in 'Title', with: @todo.title
+    fill_in 'Description', with: @todo.description
+    fill_in 'Deadline', with: @todo.deadline
 
-    assert_text "Todo was successfully created"
-    click_on "Back"
+    click_on 'Create Todo'
+
+    assert_text 'Todo was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Todo" do
+  test 'should update todo' do
     visit todo_url(@todo)
-    click_on "Edit this todo", match: :first
+    click_on 'Edit this todo', match: :first
 
-    fill_in "Deadline", with: @todo.deadline
-    fill_in "Description", with: @todo.description
-    fill_in "Finished at", with: @todo.finished_at
-    fill_in "Title", with: @todo.title
-    click_on "Update Todo"
+    fill_in 'Title', with: @todo.title
+    fill_in 'Description', with: @todo.description
+    fill_in 'Deadline', with: @todo.deadline
 
-    assert_text "Todo was successfully updated"
-    click_on "Back"
+    click_on 'Update Todo'
+
+    assert_text 'Todo was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Todo" do
+  test 'should destroy todo' do
     visit todo_url(@todo)
-    click_on "Destroy this todo", match: :first
+    click_on 'Destroy', match: :first
 
-    assert_text "Todo was successfully destroyed"
+    assert_text 'Todo was successfully destroyed'
   end
 end
