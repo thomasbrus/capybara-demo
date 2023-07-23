4d0d881bbf113cc6a8f090eea7ea28ef1bd9bfd4
Thomas Brus <thomas@bookingexperts.com>
2023-07-23 10:50:11 +0200

    Set up Tailwind forms styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+  content: ["./app/views/**/*.html.erb", "./app/helpers/**/*.rb", "./app/assets/stylesheets/**/*.css", "./app/javascript/**/*.js"],
+  theme: {
+    extend: {
+      textColor: {
+        muted: "#6b7280",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
